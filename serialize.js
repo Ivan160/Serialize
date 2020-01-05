bf79cd58a74fd07e623b8dadd3ab8cdcafeae243
Ivan160 <56685399+Ivan160@users.noreply.github.com>
2020-01-06 02:19:50 +1000

    Update serialize.js

--- serialize.js
+++ serialize.js
@@ -1,61 +1,18 @@
 var serialize = function (form) {
-	if (!form || form.nodeName !== "FORM") {
-		return;
-	}
-	var i, j, q = [];
-	for (i = form.elements.length - 1; i >= 0; i = i - 1) {
-		if (form.elements[i].name === "") {
-			continue;
-		}
-		switch (form.elements[i].nodeName) {
-			case 'INPUT':
-				switch (form.elements[i].type) {
-					case 'text':
-					case 'hidden':
-					case 'password':
-					case 'button':
-					case 'reset':
-					case 'submit':
-						q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
-						break;
-					case 'checkbox':
-					case 'radio':
-						if (form.elements[i].checked) {
-							q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
-						}
-						break;
-					case 'file':
-						break;
-				}
-				break;
-			case 'TEXTAREA':
-				q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
-				break;
-			case 'SELECT':
-				switch (form.elements[i].type) {
-					case 'select-one':
-						q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
-						break;
-					case 'select-multiple':
-						for (j = form.elements[i].options.length - 1; j >= 0; j = j - 1) {
-							if (form.elements[i].options[j].selected) {
-								q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].options[j].value));
-							}
-						}
-						break;
-				}
-				break;
-			case 'BUTTON':
-				switch (form.elements[i].type) {
-					case 'reset':
-					case 'submit':
-					case 'button':
-						q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
-						break;
-				}
-				break;
+	var q = [];
+	for (var i = 0; i < form.elements.length; i++) {
+		var elem = form.elements[i];
+		if (!elem.name || elem.type === 'file' || elem.type === 'reset' || elem.type === 'submit' || elem.type === 'button') continue;
+		if (elem.type === 'select-multiple') {
+
+			for (var n = 0; n < elem.options.length; n++) {
+				if (elem.options[n].selected) q.push(elem.name + "=" + encodeURIComponent(elem.options[n].value));
+			}
+
+		} else if ((elem.type !== 'checkbox' && elem.type !== 'radio') || elem.checked) {
+			q.push(elem.name + "=" + encodeURIComponent(elem.value));
 		}
 	}
 	return q.join("&");
 };
-// export default serialize;
+export default serialize;
