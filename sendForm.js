580cd0042b1860f9b9a8e6b2bd809cc72ed88ce9
Ivan160 <56685399+Ivan160@users.noreply.github.com>
2020-01-06 02:19:22 +1000

    Update sendForm.js

--- sendForm.js
+++ sendForm.js
@@ -7,13 +7,16 @@ var sendForm = function (url) {
          var target = event.target;
          target.querySelector('[type="submit"]').setAttribute('disabled', 'true');
 
-         var style = document.createElement('style');
-         style.textContent = `
+         if (!document.querySelector('#sendForm')) {
+            var style = document.createElement('style');
+            style.id = 'sendForm';
+            style.textContent = `
             .messageLoad {position: relative; padding: .75rem 1.25rem; margin: 1rem 0; border: 1px solid transparent; border-radius: .25rem; color: #007bff; background-color: #C9E0F9; border-color: #90BAE7;}
             .messageSuccess {position: relative; padding: .75rem 1.25rem; margin: 1rem 0; border: 1px solid transparent; border-radius: .25rem; color: #155724; background-color: #d4edda; border-color: #c3e6cb;}
             .messageError {position: relative; padding: .75rem 1.25rem; margin: 1rem 0; border: 1px solid transparent; border-radius: .25rem; color: #721c24; background-color: #f8d7da; border-color: #f5c6cb;}
             `;
-         document.head.appendChild(style);
+            document.head.appendChild(style);
+         }
 
          var messageStatus = document.createElement('div');
          messageStatus.className = 'messageLoad';
@@ -30,16 +33,19 @@ var sendForm = function (url) {
             if (request.readyState === XMLHttpRequest.DONE && request.status !== 200) {
                messageStatus.innerText = 'Упс, что-то пошло не так...';
                messageStatus.className = 'messageError';
-               setTimeout(function(){messageStatus.style.display='none'}, 10000);
+               setTimeout(function () {
+                  messageStatus.style.display = 'none';
+               }, 10000);
             }
 
             if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
                messageStatus.innerText = 'Форма успешно отправлена!';
                messageStatus.className = 'messageSuccess';
-               setTimeout(function(){messageStatus.style.display='none'}, 10000);
+               setTimeout(function () {
+                  messageStatus.style.display = 'none';
+               }, 10000);
             }
          };
-
          target.reset();
       });
    }
